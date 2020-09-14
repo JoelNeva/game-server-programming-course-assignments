a5836f9810c7faf8cf252c4827d2f97c5ed99a3f
JoelNe <joel.nevala@gmail.com>
2020-09-14 08:53:25 +0300

    Assingment 3 not ready

---
+++ Assignments/Assingment 3/IRepository.cs
@@ -0,0 +1,8 @@
+public interface IRepository
+{
+    Task<Player> Get(Guid id);
+    Task<Player[]> GetAll();
+    Task<Player> Create(Player player);
+    Task<Player> Modify(Guid id, ModifiedPlayer player);
+    Task<Player> Delete(Guid id);
+}
